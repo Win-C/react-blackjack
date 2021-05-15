f5df6ccfa3cc2a4036120dca94eaae2d7aa906f3
Winnie Chou <winnie.chou.mb@gmail.com>
2021-05-15 14:35:02 -0500

    updated app to handle multiple dealer plays

--- src/Game.js
+++ src/Game.js
@@ -20,52 +20,58 @@ function Game() {
   const [dealerHand, setDealerHand] = useState([]);
   const [playerHand, setPlayerHand] = useState([]);
   const [msg, setMsg] = useState("");
-  console.debug("dealerHand = ", dealerHand);
-  console.debug("playerHand = ", playerHand);
-  console.debug("msg = ", msg);
+  // console.debug("dealerHand = ", dealerHand);
+  // console.debug("playerHand = ", playerHand);
+  // console.debug("msg = ", msg);
 
-  function startGame(){
+  function startGame() {
     drawAndShowDealerCard();
     drawAndShowPlayerCard();
     drawAndShowPlayerCard();
   }
 
-  function drawAndShowPlayerCard(){
+  function drawAndShowPlayerCard() {
     let card = deck.pop();
     setPlayerHand(hand => [...hand, card]);
     setDeck(deck);
   }
 
-  function drawAndShowDealerCard(){
+  function drawAndShowDealerCard() {
     let card = deck.pop();
     setDealerHand(hand => [...hand, card]);
     setDeck(deck);
   }
-  
-  function handleHit(){
+
+  function handleHit() {
     drawAndShowPlayerCard();
   }
-  
-  function handleStand(){
-    // console.log(dealerScore);
-    // while(dealerScore < 17){
-      drawAndShowDealerCard();
-    // }
+
+  // TODO: Refactor code below - ideally show cards one at a time
+  function handleStand() {
+    let score = getHandScore(dealerHand);
+    let newDealerCards = dealerHand.slice();
+    while (score < 17) {
+      let card = deck.pop();
+      newDealerCards.push(card);
+      score = getHandScore(newDealerCards);
+    }
+
+    setDealerHand(newDealerCards);
   }
 
-  useEffect(function getScore(){
+  useEffect(function getScore() {
     let dealerSum = getHandScore(dealerHand);
     let playerSum = getHandScore(playerHand);
 
-    if (dealerHand.length < 2){
+    if (dealerHand.length < 2) {
       if (playerSum === 21) setMsg("Player wins: " + playerSum);
       if (playerSum > 21) setMsg("Player busts: " + playerSum);
     } else {
       if (dealerSum > 21) {
         setMsg("Dealer busts");
-      } else if (dealerSum > playerSum){
+      } else if (dealerSum > playerSum) {
         setMsg("Dealer wins");
-      } else if (playerSum > dealerSum){
+      } else if (playerSum > dealerSum) {
         setMsg("Player wins");
       } else {
         setMsg("Tie");
@@ -74,15 +80,17 @@ function Game() {
   }, [dealerHand, playerHand])
 
   return (
-    <div className = "Game">
-      {playerHand.length === 0? <button id="start" onClick={startGame}>Start Game</button> : null}
-      <Hand cards={dealerHand}/>
-      <Hand cards={playerHand}/>
+    <div className="Game">
+      {playerHand.length === 0
+        ? <button id="start" onClick={startGame}>Start Game</button>
+        : null}
+      <Hand cards={dealerHand} />
+      <Hand cards={playerHand} />
       {!msg && <p>
         <button id="hit" onClick={handleHit}>Hit</button>
         <button id="stand" onClick={handleStand}>Stand</button>
       </p>}
-      {msg && <Message msg = {msg} />}
+      {msg && <Message msg={msg} />}
     </div>
   )
 }
